06348d0b817afd249bd9c12e1973779f63032dee
xianhaoni <xianhaoni@gmail.com>
2020-06-20 10:41:40 +0800

    add gitignore

--- src/main/java/com/thoughtworks/gtb/myquiz/controller.java
+++ src/main/java/com/thoughtworks/gtb/myquiz/controller.java
@@ -1,15 +1,12 @@
 package com.thoughtworks.gtb.myquiz;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class controller {
-
+    @CrossOrigin
     @GetMapping("/:{id}")
     public String getUserName(@PathVariable int id){
         return "hello,this is xianhao";
